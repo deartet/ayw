75a5523b27a07b63b624458045e491057a8ee49b
Ethan Huang <deartet.design@gmail.com>
2016-12-09 07:00:50 +0800

    Initial Commit

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // Fixing 'use strict'
+    'use strict';
+
+    // Comparing with == generates a warining
+    if ('testing' === 'testing') {
+      console.log($);
+    }
+  });
+
+
+function add(num1, num2) {
+  return num1 * num2;
+}
---
+++ test/test.js
@@ -0,0 +1,12 @@
+//describe('This test', function(){
+//  it('should always return true', function(){
+//    expect(true).toBe(true);
+//  });
+//});
+
+describe('Add', function(){
+  it('should add two numbers together', function(){
+    expect(add(1,2)).toBe(3);
+    expect(add(3,6)).toBe(9);
+  })
+});
