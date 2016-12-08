0cd2398662bdf0fdbfeb0df3a2e1fd8c22916c1e
Ethan Huang <deartet.design@gmail.com>
2016-12-09 07:59:10 +0800

    cancel test.js

--- test/test.js
+++ test/test.js
@@ -3,10 +3,10 @@
 //    expect(true).toBe(true);
 //  });
 //});
-
-describe('Add', function(){
-  it('should add two numbers together', function(){
-    expect(add(1,2)).toBe(3);
-    expect(add(3,6)).toBe(9);
-  })
-});
+//
+//describe('Add', function(){
+//  it('should add two numbers together', function(){
+//    expect(add(1,2)).toBe(3);
+//    expect(add(3,6)).toBe(9);
+//  })
+//});
