48225a0e6a689fc1167006552615d9fcd9ba5f16
Ethan Huang <deartet.design@gmail.com>
2016-12-09 09:17:49 +0800

    ajust main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-    // Fixing 'use strict'
-    'use strict';
-
-    // Comparing with == generates a warining
-    if ('testing' === 'testing') {
-      console.log($);
-    }
-  });
-
+//$(document).ready(function() {
+//    // Fixing 'use strict'
+//    'use strict';
+//
+//    // Comparing with == generates a warining
+//    if ('testing' === 'testing') {
+//      console.log($);
+//    }
+//  });
+//
 
 function add(num1, num2) {
   return num1 + num2;
