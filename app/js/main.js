afb1448b4e37b40c327743fc0d4974c57bfa0162
Ethan Huang <deartet.design@gmail.com>
2016-12-09 13:42:11 +0800

    Fix JS and break SCSS

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,13 @@
-//$(document).ready(function() {
-//    // Fixing 'use strict'
-//   // 'use strict';
-//
-//    // Comparing with == generates a warining
-//    if ('testing' === 'testing') {
-//      console.log($);
-//    }
-//  });
+$(document).ready(function() {
+    // Comparing with == generates a warining
+    if ('testing' === 'testing') {
+      console.log($);
+    }
+  });
 
 
 function add(num1, num2) {
+   // Fixing 'use strict'
+    'use strict';
   return num1 + num2;
 }
