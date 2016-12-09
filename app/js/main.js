cbb19565ca9ca9cb1354465c83fc271e948ce7b2
Ethan Huang <deartet.design@gmail.com>
2016-12-09 12:33:12 +0800

    install in another way

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-  'use strict';
+    // Fixing 'use strict'
+    'use strict';
 
-  if ('testing' == 'testing') {
-    // Note: Cannot have console log in tests
-    // or Karma will return an error
-    // console.log('testing'));
-  }
-});
+    // Comparing with == generates a warining
+    if ('testing' === 'testing') {
+      console.log($);
+    }
+  });
 
-// function add(int1, int2) {
-//   'use strict';
-//   return int1 + int2;
-// }
+
+function add(num1, num2) {
+  return num1 + num2;
+}
